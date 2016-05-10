6b4ca1f3a148e285a0cd49258b19a5545d6ae05c
Ted L <jllts@hotmail.com>
2016-05-10 19:33:44 +0800

    draft 3 added camera

--- SavoryPlus/NoteDetailViewController.swift
+++ SavoryPlus/NoteDetailViewController.swift
@@ -7,32 +7,50 @@
 //
 
 import UIKit
+import Social
 
-class NoteDetailViewController: UIViewController {
+class NoteDetailViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
     
     @IBOutlet weak var labelTemporature: UILabel!
     @IBOutlet weak var labelTimer: UILabel!
     @IBOutlet weak var labelTag: UILabel!
     @IBOutlet weak var imageFood: UIImageView!
     
+    @IBOutlet weak var retakeButOut: UIButton!
     var note: [NoteData] = []
     var tagtag: Int = 0
     
     // MARK: Here herererere
     @IBAction func shareBut(sender: AnyObject) {
-
-        let defaultText = "This looks good !"
-        if let imageToShare = UIImage(named: self.note[tagtag].image) {
-            let activityController = UIActivityViewController(activityItems: [defaultText, imageToShare], applicationActivities: nil)
-            self.presentViewController(activityController, animated: true, completion: nil)
-        }
-
-        
+        let controller = SLComposeViewController(forServiceType:
+            SLServiceTypeFacebook)
+        controller.addImage(self.imageFood.image)
+        controller.setInitialText("This looks delicious!")
+        self.presentViewController(controller, animated: true,
+                                   completion: nil)
     }
     
     @IBAction func setBut(sender: AnyObject) {
         
     }
+    
+    @IBAction func retakePhoto(sender: AnyObject) {
+        let imagePicker = UIImagePickerController()
+        imagePicker.sourceType = .Camera
+        imagePicker.delegate = self
+        self.presentViewController(imagePicker, animated: true, completion: nil)
+    }
+    
+    func imagePickerController(picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : AnyObject]) {
+        print("info \(info)")
+        let image = info["UIImagePickerControllerOriginalImage"]
+        self.imageFood.image = image as? UIImage
+        
+        UIImageWriteToSavedPhotosAlbum(self.imageFood.image!, nil, nil, nil)
+        
+        self.dismissViewControllerAnimated(true, completion: nil)
+    }
+
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -41,6 +59,10 @@ class NoteDetailViewController: UIViewController {
         self.labelTemporature.text = self.note[tagtag].temperature
         self.labelTimer.text = self.note[tagtag].timer
         self.labelTag.text = self.note[tagtag].tag
+        
+        retakeButOut.layer.cornerRadius = 5
+        retakeButOut.layer.borderWidth = 1
+        retakeButOut.layer.borderColor = UIColor.init(red: 245/255, green: 166/255, blue: 35/255, alpha: 1).CGColor
     }
 
     override func didReceiveMemoryWarning() {
